f2996affabd7688d368ecf1126b98267bc6d0190
Gregory White <greg@gwvt.me>
2019-07-19 00:05:06 -0400

    add modal component

---
+++ src/components/AppModal.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="modal-mask">
+  <div class="modal-container">
+    <p>
+      Are you sure you want to delete this log?
+    </p>
+    <div class="row-modal-buttons">
+      <app-button :onClick="confirmDelete">
+        <font-awesome-icon :icon="['far', 'check-circle']" />
+        Confirm
+      </app-button>
+      <app-button :onClick="cancelDelete">
+        <font-awesome-icon :icon="['fas', 'ban']" />
+        Cancel
+      </app-button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import AppButton from '@/components/AppButton.vue';
+
+export default {
+  name: 'AppModal',
+  components: {
+    AppButton,
+  },
+  methods: {
+    ...mapActions([
+      'cancelDelete',
+      'confirmDelete',
+    ]),
+  },
+};
+</script>
