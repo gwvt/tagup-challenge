602f806cf2d7a88e4e316b3ee6aec33b80ef447b
Gregory White <greg@gwvt.me>
2019-07-19 00:04:36 -0400

    add button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,22 @@
+<template>
+<button @click="onClick">
+  <slot />
+</button>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'AppButton',
+  props: {
+    onClick: Function,
+  },
+  computed: {
+    ...mapState([
+      'newLogMessage',
+      'newLogTitle',
+    ]),
+  },
+};
+</script>
