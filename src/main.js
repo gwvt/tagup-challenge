d43ef80331a583ecc3444426147ee07256ab46e7
Gregory White <greg@gwvt.me>
2019-07-19 00:06:22 -0400

    add icons to app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCheckCircle, faCommentAlt } from '@fortawesome/free-regular-svg-icons';
+import { faBan } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import App from './App.vue';
 import store from './store';
+
+[
+  faBan,
+  faCheckCircle,
+  faCommentAlt,
+].forEach((i) => {
+  library.add(i);
+});
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
