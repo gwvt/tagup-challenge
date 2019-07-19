35ed00ed24010fd33e9f4efaa922514191f3120e
Gregory White <greg@gwvt.me>
2019-07-19 09:01:10 -0400

    clean up

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -5,18 +5,10 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
-
 export default {
   name: 'AppButton',
   props: {
     onClick: Function,
-  },
-  computed: {
-    ...mapState([
-      'newLogMessage',
-      'newLogTitle',
-    ]),
   },
 };
 </script>
